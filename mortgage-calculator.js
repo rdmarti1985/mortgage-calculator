92a4fab4a46189859a2b8414a5378dfc21000364
rdmarti1985 <130017311+rdmarti1985@users.noreply.github.com>
2023-04-05 18:58:11 -0700

    Update mortgage-calculator.js

--- mortgage-calculator.js
+++ mortgage-calculator.js
@@ -1,58 +1,65 @@
-document.getElementById("mortgage-form").addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const homePrice = parseFloat(document.getElementById("home-price").value);
-  const downPayment = parseFloat(document.getElementById("down-payment").value);
-  const downPaymentPercentage = parseFloat(document.getElementById("down-payment-percentage").value);
-  const loanAmount = parseFloat(document.getElementById("loan-amount").value);
-  const loanTerm = parseFloat(document.getElementById("loan-term").value);
-  const interestRate = parseFloat(document.getElementById("interest-rate").value) / 100;
-  const propertyTax = parseFloat(document.getElementById("property-tax").value) / 100;
-  const homeInsurance = parseFloat(document.getElementById("home-insurance").value);
-  const pmi = parseFloat(document.getElementById("pmi").value) / 100;
-
-  const principal = loanAmount;
-  const annualInterestRate = interestRate;
-  const numberOfPayments = loanTerm * 12;
-
-  const monthlyInterestRate = annualInterestRate / 12;
-  const monthlyPayment = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
-
-  const totalInterest = (monthlyPayment * numberOfPayments) - principal;
-  const annualPropertyTax = propertyTax * homePrice;
-  const monthlyPropertyTax = annualPropertyTax / 12;
-  const monthlyHomeInsurance = homeInsurance / 12;
-  const monthlyPMI = loanAmount * pmi / 12;
-  const totalMonthlyPayment = monthlyPayment + monthlyPropertyTax + monthlyHomeInsurance + monthlyPMI;
-
-  document.getElementById("results").innerHTML = `
-    <p>Monthly Payment: $${monthlyPayment.toFixed(2)}</p>
-    <p>Total Interest: $${totalInterest.toFixed(2)}</p>
-    <p>Total Monthly Payment (including taxes, insurance, and PMI): $${totalMonthlyPayment.toFixed(2)}</p>
-  `;
-
-  const scheduleTable = document.createElement("table");
-  let tableHeader = "<tr><th>Month</th><th>Interest</th><th>Principal</th><th>Remaining Balance</th></tr>";
-
-  let remainingBalance = principal;
-  let tableRows = "";
-
-  for (let i = 1; i <= numberOfPayments; i++) {
-    const interestPayment = remainingBalance * monthlyInterestRate;
-    const principalPayment = monthlyPayment - interestPayment;
-    remainingBalance -= principalPayment;
-
-    tableRows += `
-      <tr>
-        <td>${i}</td>
-        <td>$${interestPayment.toFixed(2)}</td>
-        <td>$${principalPayment.toFixed(2)}</td>
-        <td>$${remainingBalance.toFixed(2)}</td>
-      </tr>
-    `;
+  if (!homePrice || !downPayment || !downPaymentPercentage || !loanAmount || !loanTerm || !interestRate || !propertyTax || !homeInsurance || !pmi) {
+    document.getElementById("results").innerHTML = "Please fill in all fields.";
+    return;
   }
 
-  scheduleTable.innerHTML = tableHeader + tableRows;
-  document.getElementById("amortization-schedule").innerHTML = "<h3>Amortization Schedule</h3>";
-  document.getElementById("amortization-schedule").appendChild(scheduleTable);
+  const monthlyInterestRate = interestRate / 12;
+  const totalPayments = loanTerm * 12;
+  const principal = loanAmount;
+  const monthlyPropertyTax = (homePrice * propertyTax) / 12;
+  const monthlyHomeInsurance = homeInsurance / 12;
+
+  const p = principal;
+  const i = monthlyInterestRate;
+  const n = totalPayments;
+
+  const monthlyPayment = calculateMonthlyPayment(p, i, n);
+  const totalCost = monthlyPayment * totalPayments;
+  const totalInterest = totalCost - principal;
+  const monthlyPaymentWithTaxes = monthlyPayment + monthlyPropertyTax + monthlyHomeInsurance;
+
+  document.getElementById("results").innerHTML =
+    `Monthly Payment: $${monthlyPayment.toFixed(2)}<br>` +
+    `Monthly Payment (with taxes and insurance): $${monthlyPaymentWithTaxes.toFixed(2)}<br>` +
+    `Total Cost: $${totalCost.toFixed(2)}<br>` +
+    `Total Interest: $${totalInterest.toFixed(2)}`;
+
+  const amortizationSchedule = calculateAmortizationSchedule(p, i, n);
+  const tableRows = amortizationSchedule.map((row) => {
+    return `<tr><td>${row.paymentNumber}</td><td>$${row.payment.toFixed(2)}</td><td>$${row.interest.toFixed(2)}</td><td>$${row.principal.toFixed(2)}</td><td>$${row.balance.toFixed(2)}</td></tr>`;
+  });
+  const tableHeader = "<tr><th>Payment Number</th><th>Payment</th><th>Interest</th><th>Principal</th><th>Balance</th></tr>";
+  document.getElementById("amortization-schedule").innerHTML =
+    "<h2>Amortization Schedule</h2>" +
+    "<table>" +
+    tableHeader +
+    tableRows.join("") +
+    "</table>";
 });
+
+function calculateMonthlyPayment(principal, monthlyInterestRate, totalPayments) {
+  const x = Math.pow(1 + monthlyInterestRate, totalPayments);
+  return (principal * x * monthlyInterestRate) / (x - 1);
+}
+
+function calculateAmortizationSchedule(principal, monthlyInterestRate, totalPayments) {
+  const payment = calculateMonthlyPayment(principal, monthlyInterestRate, totalPayments);
+  let balance = principal;
+  const amortizationSchedule = [];
+
+  for (let i = 0; i < totalPayments; i++) {
+    const interest = balance * monthlyInterestRate;
+    const paidPrincipal = payment - interest;
+    balance -= paidPrincipal;
+
+    amortizationSchedule.push({
+      paymentNumber: i + 1,
+      payment,
+      interest,
+      principal: paidPrincipal,
+      balance,
+    });
+  }
+
+  return amortizationSchedule;
+}
